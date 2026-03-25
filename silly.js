e63f0a8e8df4843f1f40f3f074b9149162535f1f
CARLOSYnaC <b2029yccarlos@pshs.edu.ph>
2026-03-25 20:48:00 +0800

    Implement movie ratings storage and display

--- silly.js
+++ silly.js
@@ -1,1 +1,37 @@
-const movies = []
+ let movieRatings = JSON.parse(localStorage.getItem("movieRatings"));
+
+function showMovies() {
+  const list = document.getElementById("movieList");
+    list.innerHTML = "";
+    for (let key in movieRatings) {
+      const char = movieRatings[key];
+      const div = document.createElement("div");
+      div.className = "movie";
+      div.innerHTML = `
+        <strong>${movie.name}</strong> (Year: ${movie.year}
+        <button class="delete-btn" onclick="removeMovie('${key}')">Delete</button>
+        `;
+      list.appendChild(div);
+    }
+}
+
+document.getElementById("animeForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+    const key = document.getElementById("key").value.trim();
+    const name = document.getElementById("name").value.trim();
+    const year = document.getElementById("year").value.trim();
+
+movieRatings[key] = {name, year};
+
+localStorage.setItem("movieRatings", JSON.stringify(movieRatings));
+showMovies();
+this.reset();
+  });
+
+function removeMovie(key) {
+  delete movieRatings[key];
+  localStorage.setItem("movieRatings", JSON.stringify(movieRatings));
+  showMovies();
+}
+
+    showMovies();
